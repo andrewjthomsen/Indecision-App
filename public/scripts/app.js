47e844fb6608da32995260f86a30d2a5a06d6586
andrew <andrewjthomsen@yahoo.com>
2019-07-03 20:53:41 -0700

    Made changes to src folder's app.js and scripts folder's app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,57 @@
-"use strict";
+'use strict';
 
-console.log("App.js is running");
+console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  "Indecision App"
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Andrew Thomsen'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 31'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: California'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
