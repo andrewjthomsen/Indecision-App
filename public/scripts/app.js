e29e5c88128c45f0a6f839a10476146051eaedff
andrew <andrewjthomsen@yahoo.com>
2019-07-05 23:09:43 -0700

    Working on es6 syntax to be integrated into indecision app next

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,22 @@
 var nameVar = 'Andrew';
 nameVar = 'Mike';
 console.log('nameVar', nameVar);
+
+var nameLet = 'Jen';
+nameLet = 'Julie';
+console.log('nameLet', nameLet);
+
+var nameConst = 'Frank';
+console.log('nameconst', nameConst);
+
+// Block Scoping -- acessible in braces
+
+var fullName = 'Andrew Mead';
+var firstName = void 0;
+
+if (fullName) {
+    var _firstName = fullName.split(' ')[0];
+    console.log(_firstName);
+}
+
+console.log(firstName);
