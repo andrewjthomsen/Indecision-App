c195e9ab80e8a5040d18f4fd71f690502ea162d7
andrew <andrewjthomsen@yahoo.com>
2019-07-05 23:49:10 -0700

    Working on arrow functions part2 file in playground folder to be later integrated into indecision app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,40 @@
-"use strict";
+'use strict';
 
-function square(x) {
-    return x * x;
+// arguments object - no longer bound with arrow functions
+
+var add = function add(a, b) {
+  // console.log(arguments);
+  return a + b;
+};
+console.log(add(55, 1, 1001));
+
+// this keyword - no longer bound
+
+var user = {
+  name: 'Andrew',
+  cities: ['Philadelphia', 'New York', 'Dublin'],
+  printPlacesLived: function printPlacesLived() {
+    var _this = this;
+
+    return this.cities.map(function (city) {
+      return _this.name + ' has lived in ' + city;
+    });
+  }
+};
+console.log(user.printPlacesLived());
+
+// Challenge area
+
+var multiplier = {
+  numbers: [10, 20, 30],
+  multiplyBy: 3,
+  multiply: function multiply() {
+    var _this2 = this;
+
+    return this.numbers.map(function (number) {
+      return number * _this2.multiplyBy;
+    });
+  }
 };
 
-console.log(square(3));
-
-var squareArrow = function squareArrow(x) {
-    return x * x;
-};
-console.log(squareArrow(9));
+console.log(multiplier.multiply());
--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -1,10 +1,19 @@
-function square(x) {
-    return x * x;
-};
+// function square(x) {
+//     return x * x;
+// };
 
-console.log(square(3));
+// console.log(square(3));
 
-const squareArrow = (x) => {
-    return x * x;
-};
-console.log(squareArrow(9));
+// // const squareArrow = (x) => {
+// //     return x * x;
+// // };
+
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(9));
+// const getFirstName = (fullName) => {
+//     return fullName.split(' ')[0];
+// };
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
+console.log(getFirstName('Andrew Thomsen'));
