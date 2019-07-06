10457de2f010e18fc1606fd2367945867a43f9a6
andrew <andrewjthomsen@yahoo.com>
2019-07-05 22:57:28 -0700

    Working on es6 syntax to be integrated into indecision app next

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 console.log("App.js is running!");
-// If statements 
-// Ternary operators
-// Logical and operator
-
-// Only render the subtitle (and p tag) if subtitle exists - logical and operator
-// Render new p tag - if options.length > 0 m'here are your options 'no options'
 
 var app = {
-  title: 'Indecision App',
-  options: []
+  title: "Indecision App",
+  subtitle: "Template React App",
+  options: ["one", "two"]
 };
 var template = (
   <div>
     <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'no options'}</p>
+    <p>{app.options.length > 0 ? "Here are your options" : "no options"}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -22,23 +17,21 @@ var template = (
   </div>
 );
 var user = {
-  name: 'Andrew',
+  name: "Andrew",
   age: 31,
-  location: 'San Diego, California'
+  location: "San Diego, California"
 };
-// If statement practice
+
 function getLocation(location) {
- if(location) {
-   return <p>location: {location}</p>;
- } 
+  if (location) {
+    return <p>location: {location}</p>;
+  }
 }
-// Ternary operator practice
-// Logical and operator practice
 var templateTwo = (
   <div>
-    <h1>{user.name ? user.name : 'anonymous'}</h1>
-   {(user.age && user.age >= 18) &&  <p>Age: {user.age}</p>}
-  
+    <h1>{user.name ? user.name : "anonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+
     {getLocation(user.location)}
   </div>
 );
