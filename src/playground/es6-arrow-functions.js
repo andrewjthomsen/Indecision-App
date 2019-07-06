244f570123ce07c96bccd62c804ec9d1e1c5256e
andrew <andrewjthomsen@yahoo.com>
2019-07-05 23:33:01 -0700

    Working on arrow functions in playground folder to be later integrated into indecision app

--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -1,12 +1,10 @@
-"use strict";
-
 function square(x) {
     return x * x;
 };
 
 console.log(square(3));
 
-var squareArrow = function squareArrow(x) {
+const squareArrow = (x) => {
     return x * x;
 };
 console.log(squareArrow(9));
