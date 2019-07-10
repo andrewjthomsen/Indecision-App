25e5454e61e8506e3347dbadf8412ab3dd6cd0af
andrew <andrewjthomsen@yahoo.com>
2019-07-09 20:39:28 -0700

    Further built out es6 class examples in playground folder

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -14,13 +14,42 @@ class Person {
 
 class Student extends Person {
   constructor(name, age, major) {
-      super(name, age);
-      this.major = major;
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+        description += ` Their major is ${this.major}.`;
+      }
+  
+      return description;
   }
 }
 
-const me = new Student("Andrew Thomsen", 26, "Web Development");
-console.log(me.getDescription());
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+      super(name, age);
+      this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+      let greeting = super.getGreeting();
+  
+      if (this.homeLocation) {
+        greeting += ` I am visiting from ${this.homeLocation}.`;
+      }
+  
+      return greeting;
+    }
+  }
 
-const other = new Student();
-console.log(other);
+  const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+  console.log(me.getGreeting());
+  
+  const other = new Traveler(undefined, undefined, 'Nowhere');
+  console.log(other.getGreeting());
+  
