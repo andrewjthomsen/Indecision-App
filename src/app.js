85eb874cf13c53346004622c979c3f40d7462895
andrew <andrewjthomsen@yahoo.com>
2019-07-11 22:26:25 -0700

    Began working on adding react class components

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 class IndecisionApp extends React.Component {
   render() {
     const title = 'Indecision';
-    const subtitle= 'Put your life in the hands of a computer. Obey the computer!';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['Thing one', 'Thing two', 'Thing four'];
+
     return (
       <div>
-        <Header title={title} subtitle={subtitle}/>
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
@@ -38,8 +40,9 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        Options component here
-        <Option />
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
       </div>
     );
   }
@@ -49,7 +52,7 @@ class Option extends React.Component {
   render() {
     return (
       <div>
-        Option component here
+        {this.props.optionText}
       </div>
     );
   }
