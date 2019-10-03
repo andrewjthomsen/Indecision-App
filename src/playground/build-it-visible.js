2d0ab1dd9050b17cf9bf20444232d4ec19fe121a
andrew <andrewjthomsen@yahoo.com>
2019-10-02 17:57:35 -0700

    Built out project scaffold, added state to app.js

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,33 @@
+class VisibilityToggle extends React.Component {
+  render() {
+    return <p>Test</p>;
+  }
+}
+
+ReactDom.render(<VisibilityToggle/>, document.getElementById('app'))
+// let visibility = false;
+
+// const toggleVisibility = () => {
+//   visibility = !visibility;
+//   render();
+// };
+
+// const render = () => {
+//   const jsx = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button onClick={toggleVisibility}>
+//         {visibility ? 'Hide details' : 'Show details'}
+//       </button>
+//       {visibility && (
+//         <div>
+//           <p>Hey. These are some details you can now see!</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+
+//   ReactDOM.render(jsx, document.getElementById('app'));
+// };
+
+// render();
